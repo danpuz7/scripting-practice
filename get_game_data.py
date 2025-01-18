17c1f434292f0c9c30eb4696171503fa879ddd7e
Daniel Puzan <danpuz7@gmail.com>
2025-01-18 10:43:06 -0600

    scripting

--- get_game_data.py
+++ get_game_data.py
@@ -1,14 +1,12 @@
 import os
 import json
-import shutil
+import shutil 
 from subprocess import PIPE, run
 import sys
-
 
 GAME_DIR_PATTERN = "game"
 GAME_CODE_EXTENSION = ".go"
 GAME_COMPILE_COMMAND = ["go", "build"]
-
 
 def find_all_game_paths(source):
     game_paths = []
@@ -20,9 +18,7 @@ def find_all_game_paths(source):
                 game_paths.append(path)
 
         break
-
     return game_paths
-
 
 def get_name_from_paths(paths, to_strip):
     new_names = []
@@ -33,27 +29,22 @@ def get_name_from_paths(paths, to_strip):
 
     return new_names
 
-
 def create_dir(path):
     if not os.path.exists(path):
         os.mkdir(path)
-
 
 def copy_and_overwrite(source, dest):
     if os.path.exists(dest):
         shutil.rmtree(dest)
     shutil.copytree(source, dest)
 
-
 def make_json_metadata_file(path, game_dirs):
     data = {
         "gameNames": game_dirs,
         "numberOfGames": len(game_dirs)
     }
-
     with open(path, "w") as f:
         json.dump(data, f)
-
 
 def compile_game_code(path):
     code_file_name = None
@@ -62,15 +53,13 @@ def compile_game_code(path):
             if file.endswith(GAME_CODE_EXTENSION):
                 code_file_name = file
                 break
-
         break
-
+    
     if code_file_name is None:
         return
-
+    
     command = GAME_COMPILE_COMMAND + [code_file_name]
     run_command(command, path)
-
 
 def run_command(command, path):
     cwd = os.getcwd()
@@ -99,11 +88,12 @@ def main(source, target):
     json_path = os.path.join(target_path, "metadata.json")
     make_json_metadata_file(json_path, new_game_dirs)
 
-
 if __name__ == "__main__":
     args = sys.argv
     if len(args) != 3:
-        raise Exception("You must pass a source and target directory - only.")
-
+        raise Exception("You must pass a source and target directory only.")
+    
     source, target = args[1:]
     main(source, target)
+
+
